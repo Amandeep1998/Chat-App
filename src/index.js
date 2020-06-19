359926326bbc0295a24ece026d6191fd71413ee5
Amandeep Saini <amandeepsaini336@gmail.com>
2020-06-19 11:20:44 +0530

    Fix bug

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ io.on('connection', (socket) => {
         const user = removedUser(socket.id);
 
         if(user) {
-            io.to(user.room).emit('message', generateMessage('Admin', `A ${user.username} has left!`));
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`));
             io.to(user.room).emit('roomData', {
                 room: user.room,
                 users: getUsersInRoom(user.room)
